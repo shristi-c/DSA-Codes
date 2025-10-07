18741d8115ef3fe23a7bee4448db19da57a8b991
shristi-c <loinshristi@gmail.com>
2025-10-07 15:54:26 +0530

    search in recursion

--- Recursion/LinearSerach.java
+++ Recursion/LinearSerach.java
@@ -3,11 +3,13 @@ import java.util.ArrayList;
 public class LinearSerach {
   public static void main(String[] args) {
       int arr[]={1,2,3,4,5,6,10,4,4};
-      System.out.println(find(arr,5,0));
-      System.out.println(findIndex(arr,5,0));
-      System.out.println(findIndexLast(arr,1,arr.length-1));
-      findAllIndex(arr, 4,0);
-      System.out.println(list);
+      // System.out.println(find(arr,5,0));
+      // System.out.println(findIndex(arr,5,0));
+      // System.out.println(findIndexLast(arr,1,arr.length-1));
+      // findAllIndex(arr, 4,0);
+      // System.out.println(list);
+      
+      System.out.println(findAllIndex2(arr,4,0));
   }
   static boolean find(int arr[],int target,int index){
     if(index==arr.length){
@@ -54,4 +56,26 @@ static ArrayList<Integer>list=new ArrayList<>();
   findAllIndex(arr,target,index+1);
 
  }
+
+static ArrayList<Integer>findAllIndex2(int [] arr,int target,int index){
+  ArrayList<Integer> list=new ArrayList<>();
+  
+  if(index==arr.length){
+    return list;
+
+  }
+  if(arr[index]==target){
+    list.add(index);
+  }
+  ArrayList<Integer> callfrombelow=findAllIndex2(arr, target, index+1);
+  list.addAll(callfrombelow);
+  return list;
 }
+
+
+
+
+
+}
+
+
